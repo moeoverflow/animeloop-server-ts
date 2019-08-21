2dff17fde887ed1a907dd51f51c47def998807b5
ShinCurry <shincurryyang@gmail.com>
2019-08-21 17:05:08 +0800

    update config.example.js

--- config.example.js
+++ config.example.js
@@ -23,6 +23,15 @@ const config = {
       }
     },
   },
+  bot: {
+    twitter: {
+      consumer_key:         '',
+      consumer_secret:      '',
+      access_token:         '',
+      access_token_secret:  '',
+      timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    }
+  },
   storage: {
     dir: {
       data: path.join(__dirname, 'storage', 'data'),
