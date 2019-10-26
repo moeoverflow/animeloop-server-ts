1dfc767d0222697d0a1097da19115195b9fae691
ShinCurry <shincurryyang@gmail.com>
2019-10-27 01:19:24 +0800

    fix import path

--- src/init.ts
+++ src/init.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata'
 import { Container } from 'typedi'
 import { MysqlService } from './core/services/MysqlService'
-import { MongodbService } from './core/services/MongoDBService'
+import { MongodbService } from './core/services/MongodbService'
 
 Container.get(MongodbService)
 Container.get(MysqlService)
