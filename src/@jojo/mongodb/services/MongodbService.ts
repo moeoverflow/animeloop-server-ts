cc18328e3ddaec18223fbd72eb104eef96b965a4
ShinCurry <shincurryyang@gmail.com>
2019-11-07 14:15:12 +0800

    minor fix

--- src/@jojo/mongodb/services/MongodbService.ts
+++ src/@jojo/mongodb/services/MongodbService.ts
@@ -1,6 +1,6 @@
-import { autoIncrement } from '@jojo/mongodb'
 import { Container, Service } from '@jojo/typedi'
 import mongoose from 'mongoose'
+import autoIncrement from 'mongoose-auto-increment'
 import { ConfigService } from '../../../core/services/ConfigService'
 
 mongoose.Promise = global.Promise
