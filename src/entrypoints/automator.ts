6f11a4548365f99de9e6f6074bd618f1dbccc0f4
ShinCurry <shincurryyang@gmail.com>
2019-11-08 00:18:20 +0800

    tmp remove sentry from automator

--- src/entrypoints/automator.ts
+++ src/entrypoints/automator.ts
@@ -1,11 +1,6 @@
 require("../init")
 import { Container } from '@jojo/typedi'
-import * as Sentry from '@sentry/node'
 import AutomatorRunner from '../automator/AutomatorRunner'
-import { ConfigService } from '../core/services/ConfigService'
-
-const configService: ConfigService = Container.get(ConfigService)
-Sentry.init({ dsn: configService.config.sentry.automator })
 
 const automatorRunner = Container.get(AutomatorRunner)
 automatorRunner.run().then(() => {}).catch((err) => console.error(err))
